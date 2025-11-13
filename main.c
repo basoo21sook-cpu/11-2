9f13f97ea0fea0bea2a9774807dc72934ab70163
basoo21sook-cpu <basoo21sook@gmail.com>
2025-11-13 15:23:20 +0900

    11-1

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-    int i = 300;        
-    int *pi = &i;      
-    char *pc = &i;      
+    int i = 10;
+    char c = 'a';
 
-    printf("%i, %i, %i\n", i, *pi, *pc);
+    int  *iptr;   
+    char *cptr;   
+    int  *iptr2;  
+
+    iptr  = &i;   
+    cptr  = &c;  
+    iptr2 = iptr; 
+
+    printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr));
+    printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr));
+    printf("iptr2 : %p, %i\n", iptr2, *iptr2);
     return 0;
 }
-
-
